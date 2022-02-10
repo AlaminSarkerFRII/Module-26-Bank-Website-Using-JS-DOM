67dcaf76ea6622797cf20cb0f4a96574c909c2bd
AlaminSarkerFRII <alamin.education1997@gmail.com>
2022-02-10 20:23:46 +0600

    added update balance system

--- js/banking.js
+++ js/banking.js
@@ -1,12 +1,9 @@
 // handle deposit button Event 
 
 document.getElementById('deposit-btn').addEventListener('click', function(){
-
-    //get deposited amount 
-
     const depositInput = document.getElementById('deposit-input');
-    const newDepositAmountText = depositInput.value;
-    const newDepositAmount = parseFloat(newDepositAmountText);
+    const newDepositAmountText = depositInput.value; // input value pabo
+    const newDepositAmount = parseFloat(newDepositAmountText); //integer a convert
     // console.log(depositAmount);
 
     //update deposit total
@@ -27,10 +24,42 @@ const newBalanceTotal = previousBalaceTotal + newDepositTotal;
 
 balanceTotal.innerText = newBalanceTotal;
 
-
-
     //clear the deposit input field........
     depositInput.value = '';
 
 
-})
+});
+
+// withraw Balance ..& Event Handler......................
+
+document.getElementById('withdraw-btn').addEventListener('click',function(){
+    
+    const withdrawInput = document.getElementById('withdraw-input');
+    const withdrawAmountText = withdrawInput.value;
+    const newWithdrawAmount = parseFloat(withdrawAmountText);
+
+    console.log(newWithdrawAmount);
+
+    //set withdraw total;
+
+    const withdrawTotal = document.getElementById('withdraw-total');
+    const previousWithdrawText = withdrawTotal.innerText;
+    const previousWithdrawTotal = parseFloat(previousWithdrawText);
+
+    const newWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+    withdrawTotal.innerText = newWithdrawTotal;
+
+    //update balance (main)
+
+    const balanceTotal = document.getElementById('balance-total');
+    const previousBalanceText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceText);
+    const newBalanceTotal = previousBalanceTotal - newWithdrawAmount; // main balance - withdraw amount.........
+
+    balanceTotal.innerText = newBalanceTotal; // new main balance
+
+    //clear withdraw set
+
+    withdrawInput.value = '';
+
+});
